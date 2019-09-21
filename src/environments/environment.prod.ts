b2e6676051a52d9868db9e7d853342334975f674
tushar agrawal <tusharagrawa@gmail.com>
2019-09-21 20:16:03 +0530

    Adding Firebase Dependecy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAerdyBkhc4s9y2b6t3O8keZoyoX5Fz6Uk',
+    authDomain: 'tusharagrawa-shoppingwebsite.firebaseapp.com',
+    databaseURL: 'https://tusharagrawa-shoppingwebsite.firebaseio.com',
+    projectId: 'tusharagrawa-shoppingwebsite',
+    storageBucket: '',
+    messagingSenderId: '689443416018',
+    appId: '1:689443416018:web:2bf403f671c7c367640a0b'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAerdyBkhc4s9y2b6t3O8keZoyoX5Fz6Uk',
+    authDomain: 'tusharagrawa-shoppingwebsite.firebaseapp.com',
+    databaseURL: 'https://tusharagrawa-shoppingwebsite.firebaseio.com',
+    projectId: 'tusharagrawa-shoppingwebsite',
+    storageBucket: '',
+    messagingSenderId: '689443416018',
+    appId: '1:689443416018:web:2bf403f671c7c367640a0b'
+  }
 };
 
 /*
