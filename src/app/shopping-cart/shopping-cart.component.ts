221475982e399d1b4d9cbcc560c6f892dcbfccb9
tushar agrawal <tusharagrawa@gmail.com>
2019-09-23 09:19:58 +0530

    Shopping Cart Page

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ShoppingCart } from '../models/shopping-cart';
+import { Product } from '../models/product';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingCartComponent implements OnInit {
 
+  cart: ShoppingCart = {} as ShoppingCart;
+  prod: Product = {} as Product;
+  qty: number;
+  totalPrice: number;
+
   constructor() { }
 
   ngOnInit() {
+    let count = 0;
+    let price = 0;
+    let cart1 = new ShoppingCart();
+    Object.keys(sessionStorage).forEach(function (item) {
+      if (item) {
+        let prod1 = JSON.parse(sessionStorage[item]);
+        count += prod1.quantity;
+        price += (prod1.quantity * prod1.price);
+        cart1.addProduct(prod1);
+      }
+    });
+    this.qty = count;
+    this.cart = cart1;
+    this.totalPrice = price;
   }
 
+  clearCart() {
+    sessionStorage.clear();
+  }
 }
